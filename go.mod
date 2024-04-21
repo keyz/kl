0c76da556d51b8ddb1a865665e85e5a61e5eb48e
Keyan Zhang <2268452+keyz@users.noreply.github.com>
2024-04-21 11:42:45 -0700

    + color

--- go.mod
+++ go.mod
@@ -3,13 +3,17 @@ module github.com/keyz/kl
 go 1.22.2
 
 require (
+	github.com/fatih/color v1.16.0
 	github.com/stretchr/testify v1.9.0
 	golang.design/x/hotkey v0.4.1
 )
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	golang.design/x/mainthread v0.3.0 // indirect
+	golang.org/x/sys v0.14.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"log"
+	"os"
 
+	"github.com/fatih/color"
 	"golang.design/x/hotkey"
 	"golang.design/x/hotkey/mainthread"
 )
@@ -15,15 +16,16 @@ func loop() {
 	hk := hotkey.New([]hotkey.Modifier{hotkey.ModCmd, hotkey.ModOption}, hotkey.KeyR)
 
 	if err := hk.Register(); err != nil {
-		log.Fatalf("[kl] failed to register hotkey: %v", err)
+		color.Red("[kl] failed to register hotkey: %v", err)
+		os.Exit(1)
 	}
 
-	log.Printf("[kl] listening to hotkey %v...\n", hk)
+	color.Green("[kl] listening to hotkey %v...\n", hk)
 	defer hk.Unregister()
 
 	for {
 		<-hk.Keyup()
-		log.Printf("[kl] hotkey pressed: %v\n", hk)
+		color.Green("[kl] hotkey pressed: %v\n", hk)
 	}
 }
 
