eb93270d0689124984824196c7eff065b6f62f48
Keyan Zhang <2268452+keyz@users.noreply.github.com>
2024-04-21 11:15:23 -0700

    + hotkey

--- go.mod
+++ go.mod
@@ -2,10 +2,14 @@ module github.com/keyz/kl
 
 go 1.22.2
 
-require github.com/stretchr/testify v1.9.0
+require (
+	github.com/stretchr/testify v1.9.0
+	golang.design/x/hotkey v0.4.1
+)
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	golang.design/x/mainthread v0.3.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -1,11 +1,30 @@
 package main
 
 import (
-	"fmt"
+	"log"
+
+	"golang.design/x/hotkey"
+	"golang.design/x/hotkey/mainthread"
 )
 
 func main() {
-	fmt.Printf("hello %v\n", Add(1, 2))
+	mainthread.Init(loop)
+}
+
+func loop() {
+	hk := hotkey.New([]hotkey.Modifier{hotkey.ModCmd, hotkey.ModOption}, hotkey.KeyR)
+
+	if err := hk.Register(); err != nil {
+		log.Fatalf("[kl] failed to register hotkey: %v", err)
+	}
+
+	log.Printf("[kl] listening to hotkey %v...\n", hk)
+	defer hk.Unregister()
+
+	for {
+		<-hk.Keyup()
+		log.Printf("[kl] hotkey pressed: %v\n", hk)
+	}
 }
 
 func Add(a, b int) int {
